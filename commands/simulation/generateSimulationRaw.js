2aba06e9e83e2ac1638842708cbb2358b9e1b5b5
Cauã Diego <tixyelpvd@gmail.com>
2023-12-16 02:54:55 -0300

    update

--- commands/simulation/generateSimulationRaw.js
+++ commands/simulation/generateSimulationRaw.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 
 const { createFile } = require('../../functions/manageFs')
-const { getGists, runUpdate, runCreate } = require('../../functions/gistManage')
+const { getGist, getGists, runUpdate, runCreate } = require('../../functions/gistManage')
 
 module.exports = {
   name: 'generateSimulationRaw',
@@ -16,11 +16,17 @@ module.exports = {
         'Authorization': `Bearer ${token}`,
       }
 
+    const regex = {
+      version: /\b(\d+\.\d+\.\d+)\b/g,
+      raw_pattern: /^(https?):\/\/gist\.github(?:usercontent)?\.com\/(.+?\/[0-9a-f]+\/raw\/(?:[0-9a-f]+\/)?.+)$/i,
+      raw_replace: '$1://gistcdn.githack.com/$2',
+    }
+
     let simulation = fs.readFileSync(path.dirname(fsPath) + '\\' + 'simulation.js', 'utf8')
 
     if (!simulation) return vscode.window.showInformationMessage('Você precisa estar próximo a um arquivo de simulação')
 
-    let version = simulation.match(/\b(\d+\.\d+\.\d+)\b/g)[0],
+    let version = simulation.match(regex.version)[0],
       body = JSON.stringify({
         'description': 'Simulation ' + version,
         'public': false,
@@ -48,30 +54,33 @@ module.exports = {
     }
 
     const callback = function (/** @type {Object} */ res) {
-      const { files } = res,
-        file = Object.values(files)[0],
-        { raw_url } = file
+        const { raw_url } = Object.values(res.files)[0]
 
-      let [pattern, replace] = [
-          /^(https?):\/\/gist\.github(?:usercontent)?\.com\/(.+?\/[0-9a-f]+\/raw\/(?:[0-9a-f]+\/)?.+)$/i,
-          '$1://gistcdn.githack.com/$2',
-        ],
-        githack
+        if (regex.raw_pattern.test(raw_url))
+          updateFields(raw_url.replace(regex.raw_pattern, regex.raw_replace)).then(() =>
+            vscode.window.showInformationMessage('Custom fields atualizado com sucesso!'),
+          )
+        else return vscode.window.showInformationMessage('Ocorreu algum erro...')
+      },
+      updateSimulation = async (/** @type {Object} */ item) => {
+        return await getGist(item.id, headers, async (/** @type {Object} */ res) => {
+          let { content } = Object.values(res.files)[0]
 
-      if (pattern.test(raw_url)) {
-        githack = raw_url.replace(pattern, replace)
+          version = content.match(regex.version)[0]
 
-        updateFields(githack).then(() => vscode.window.showInformationMessage('Custom fields atualizado com sucesso!'))
-      } else return vscode.window.showInformationMessage('Ocorreu algum erro...')
-    }
+          content.length && (await createFile(path.dirname(fsPath) + '\\' + 'simulation.js', content))
+
+          return callback(res)
+        })
+      }
 
     getGists(headers, async (/** @type {Array} */ res) => {
       if (Array.isArray(res)) {
-        if (res.some(({ description }) => description.includes(version))) {
-          let { id } = res.find(({ description }) => description.includes(version))
-
-          runUpdate(id, headers, body, callback)
-        } else runCreate(headers, body, callback)
+        if (res.some(({ description }) => description.match(regex.version)[0] > version))
+          updateSimulation(res.find(({ description }) => description.match(regex.version)[0] > version))
+        else if (res.some(({ description }) => description.includes(version)))
+          runUpdate(res.find(({ description }) => description.includes(version)).id, headers, body, callback)
+        else runCreate(headers, body, callback)
       } else console.error(res)
     })
   },
--- functions/gistManage.js
+++ functions/gistManage.js
@@ -1,38 +1,49 @@
 const fetch = require('node-fetch')
 
 module.exports = {
-  getGists: async (headers, callback) => {
+  getGist: async (/** @type {String} */ id, /** @type {Object} */ headers, /** @type {Function} */ callback) => {
+    // @ts-ignore
+    return await fetch('https://api.github.com/gists/' + id, {
+      method: 'GET',
+      headers,
+    })
+      .then((/** @type {fetch.Response} */ res) => res.json())
+      .then((/** @type {Object} */ res) => {
+        callback(res)
+      })
+  },
+  getGists: async (/** @type {Object} */ headers, /** @type {Function} */ callback) => {
     // @ts-ignore
     return await fetch('https://api.github.com/gists', {
       method: 'GET',
       headers,
     })
-      .then((res) => res.json())
-      .then((res) => {
+      .then((/** @type {fetch.Response} */ res) => res.json())
+      .then((/** @type {Object} */ res) => {
         callback(res)
       })
   },
-  runUpdate: async (/** @type {String} */ id, headers, body, callback) => {
+  runUpdate: async (/** @type {String} */ id, /** @type {Object} */ headers, /** @type {Object} */ body, /** @type {Function} */ callback) => {
     // @ts-ignore
     return await fetch('https://api.github.com/gists/' + id, {
       method: 'PATCH',
       headers,
       body,
     })
-      .then((res) => res.json())
-      .then((res) => {
+      .then((/** @type {fetch.Response} */ res) => res.json())
+      .then((/** @type {Object} */ res) => {
         callback(res)
       })
   },
-  runCreate: async (headers, body, callback) => {
+  runCreate: async (/** @type {Object} */ headers, /** @type {Object} */ body, /** @type {Function} */ callback) => {
     // @ts-ignore
     return await fetch('https://api.github.com/gists', {
       method: 'POST',
       headers,
       body,
     })
-      .then((res) => res.json())
-      .then((res) => {
+      .then((/** @type {fetch.Response} */ res) => res.json())
+      .then((/** @type {Object} */ res) => {
         callback(res)
       })
   },
